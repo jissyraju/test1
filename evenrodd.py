7b7d87c9be00fa65f7dcd75ed4868f87e053a716
gopika <gopika.ma.2015@gmail.com>
2019-03-25 13:13:23 +0530

    Initial commit

---
+++ evenrodd.py
@@ -0,0 +1,5 @@
+n=input("input n:")
+if(n%2==0):
+   print("n is even")
+else:
+   print("n is odd")
---
+++ largest.py
@@ -0,0 +1,13 @@
+a=input("input a:")
+b=input("inp[ut b:")
+c=input("input c:")
+if(a>b):
+   if(a>c):
+      print("a is the largest")
+   else:
+      print("c is the largest")
+else:
+   if(b>c):
+      print("b is the largest")
+   else:
+      print("c is the largest")
